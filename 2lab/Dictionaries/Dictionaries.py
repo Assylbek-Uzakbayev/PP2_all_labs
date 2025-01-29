c4e99a8c102714f7f661bc0930928436a0fa023e
Uzakbayev Assylbek <a_uzakbayev@kbtu.kz>
2025-01-29 08:38:47 +0500

    lab2

---
+++ 2lab/Dictionaries/Dictionaries.py
@@ -0,0 +1,63 @@
+#1
+vthisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#3
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#4
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#5
+print(len(thisdict))
+
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#5
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#6
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+
+
+
+
+
+
+
+
