7f6fec5a6ea2d4ed9f5e5342886aae4e7f1fd25c
Assylbek-Uzakbayev <a_uzakbayev@kbtu.kz>
2025-02-19 11:51:52 +0500

    task

---
+++ 4lab/generators/task1.py
@@ -0,0 +1,4 @@
+print("num:")
+n = int(input())
+for i in range(n + 1):
+    print(i**2)
---
+++ 4lab/generators/task4.py
@@ -0,0 +1,6 @@
+print("num 1: ")
+a = int(input())
+print("num 2: ")
+b = int(input())
+for i in range(a , b + 1):
+    print(i**2)
